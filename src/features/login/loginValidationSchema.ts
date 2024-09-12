3911db094b955e2626eee374dd68c0af4486d520
ahorn256 <anton.horn@futuristen.education>
2024-09-12 23:42:21 +0200

    fixed spelling

--- src/features/login/loginValidationSchema.ts
+++ src/features/login/loginValidationSchema.ts
@@ -2,7 +2,7 @@ import * as Yup from 'yup';
 
 const loginValidationSchema = Yup.object().shape({
   user: Yup.string().required('Benutzername ist ein Pflichtfeld'),
-  password: Yup.string().required('Passwort ist ein Prflichtfeld'),
+  password: Yup.string().required('Passwort ist ein Pflichtfeld'),
 });
 
 export default loginValidationSchema;
