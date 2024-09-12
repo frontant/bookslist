6f22f1ac3aa280c5bde2396585d172fb2d027380
ahorn256 <anton.horn@futuristen.education>
2024-09-12 23:40:41 +0200

    added token based routing

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,18 @@ import DeletionDialog from './features/books/DeletionDialog';
 import NotFound from './NotFound';
 import LoginForm from './features/login/LoginForm';
 import Nav from './features/navMenu/Nav';
+import { useAppSelector } from './app/hooks';
+import { selectToken } from './features/login/login.slice';
 
 function App() {
+  const loginToken = useAppSelector(selectToken);
+
   return (
     <div className="App">
       <Nav />
       <div className='App-content'>
         <Routes>
-          <Route path='/' element={<BooksApp />}>
+          <Route path='/' element={loginToken ? <BooksApp /> : <p>Sie müssen sich anmelden, um den Inhalt zu sehen.</p>}>
             <Route path='/new' element={<FormDialog />} />
             <Route path='/edit/:id' element={<FormDialog />} />
             <Route path='/delete/:id' element={<DeletionDialog />} />
