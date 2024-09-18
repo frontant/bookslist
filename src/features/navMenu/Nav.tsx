b420b84ea5df6120ad9bd28356b8e3830c16793a
ahorn256 <anton.horn@futuristen.education>
2024-09-18 23:32:32 +0200

    added language switcher

--- src/features/navMenu/Nav.tsx
+++ src/features/navMenu/Nav.tsx
@@ -1,23 +1,55 @@
 import * as Icon from "@mui/icons-material";
-import { AppBar, Box, IconButton, Toolbar } from "@mui/material";
+import { AppBar, Box, IconButton, Menu, MenuItem, MenuList, Toolbar } from "@mui/material";
 import { Link } from "react-router-dom";
 import { resetLoginToInitialState, selectToken } from "../login/login.slice";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { resetBooksToInitialState } from "../books/booksSlice";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 
 function Nav() {
   const loginToken = useAppSelector(selectToken);
   const dispatch = useAppDispatch();
+  const [ anchorEl, setAnchorEl ] = useState<HTMLElement|null>(null);
+  const open = Boolean(anchorEl);
+  const { i18n } = useTranslation();
 
   function onLogout() {
     dispatch(resetBooksToInitialState());
     dispatch(resetLoginToInitialState());
   }
 
+  function handleMenuOpen(event: React.MouseEvent<HTMLElement>) {
+    setAnchorEl(event.currentTarget);
+  }
+
+  function handleMenuClose() {
+    setAnchorEl(null);
+  }
+
   return (
     <AppBar position="sticky">
       <Toolbar>
+        <IconButton
+          color="inherit"
+          onClick={handleMenuOpen}>
+          <Icon.Language />
+        </IconButton>
+        <Menu
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleMenuClose}
+          keepMounted>
+          <MenuList disablePadding>
+            <MenuItem dense onClick={handleMenuClose}>
+              <Link to='?lng=de' style={{textDecoration: 'none'}} onClick={() => i18n.changeLanguage('de')}>DE</Link>
+            </MenuItem>
+            <MenuItem dense onClick={handleMenuClose}>
+              <Link to='?lng=en' style={{textDecoration: 'none'}} onClick={() => i18n.changeLanguage('en')}>EN</Link>
+            </MenuItem>
+          </MenuList>
+        </Menu>
         <Box sx={{flexGrow: 1}} />
         { !loginToken && <Link to="/login" style={{color: 'inherit'}}><Icon.Login /></Link> }
         { loginToken && <IconButton color="inherit" onClick={onLogout}><Icon.Logout /></IconButton> }
