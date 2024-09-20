0b2f5a463e57ccbe121c7c7fb2373cb389c76245
ahorn256 <anton.horn@futuristen.education>
2024-09-20 02:08:13 +0200

    translated "Not found" page

--- src/NotFound.tsx
+++ src/NotFound.tsx
@@ -1,10 +1,17 @@
+import { Trans, useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
 function NotFound() {
+  const { t } = useTranslation();
+
   return (
     <>
-      <h2 data-testid='not-found-caption'>Not Found</h2>
-      <p>weiter zur <Link to='/'>Startseite</Link></p>
+      <h2 data-testid='not-found-caption'>{t('not-found')}</h2>
+      <p>
+        <Trans i18nKey="back-to-homepage">
+          <Link to='/'>homepage</Link>
+        </Trans>
+      </p>
     </>
   );
 }
